fa19b52847f819cf0014dcfa88834fa943d1f324
ralf.ramge@ict.technology <ralf.ramge@ict.technology>
2022-02-28 13:23:35 +0100

    initial

---
+++ 5.1/Slide_04/test_instance.tf
@@ -0,0 +1,51 @@
+resource "oci_core_instance" "test_instance" {
+  count = var.is_free == true ? 1 : 3
+
+  #Required
+  availability_domain = local.list_of_ads[count.index % local.number_of_ads]
+  compartment_id      = var.compartment_id
+  shape               = var.is_production ? local.is_database : local.is_development
+  #hostname_label = "my-server-${count.index}"
+
+  create_vnic_details {
+    subnet_id      = oci_core_subnet.test_subnet.id
+    hostname_label = "my-server-${count.index}"
+    nsg_ids = [oci_core_network_security_group.test_network_security_group.id]
+  }
+
+  source_details {
+    source_id   = lookup(var.image_OL8_x64, var.region, "illegal region!")
+    source_type = "image"
+  }
+
+  metadata = {
+    ssh_authorized_keys = ( 
+                            fileexists(var.ssh_authorized_keys_path) ?  # If the authorized_keys file exists, then
+                              file(var.ssh_authorized_keys_path) :      # use this file, else
+                              (                                           # 
+                                fileexists(var.ssh_public_key_path) ?     # if the public_key file exists, then
+                                  file(var.ssh_public_key_path) :         # use this file, else
+                                  null                                    # return null
+                                )
+                            )
+  }
+
+  extended_metadata = {}
+
+  freeform_tags = local.acme_tags
+
+provisioner "remote-exec" {
+
+  connection {
+    type = "ssh"
+    host = self.public_ip
+    user = "opc"
+    private_key = "${file("~/.ssh/passwordless-id_rsa")}"
+    }
+
+    inline = [
+      "# sudo dnf -y update",                        # may take a lot of time, commented out for purpose of demo
+      "sudo dnf -y install httpd mod_ssl"
+    ]
+  }
+}
